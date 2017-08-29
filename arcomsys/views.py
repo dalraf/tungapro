415f78f80dd5bcb6cfbbe266ed6dd296ed20050b
Daniel Fonseca Alves <dalrafgmail.com>
2017-08-29 11:25:36 -0300

    Versão alpha 0.1

--- arcomsys/views.py
+++ arcomsys/views.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from .models import Cliente
+
 # Create your views here.
+
+def listarclientes(request):
+    clientes = Cliente.objects.all()
+    return render(request,'listarclientes.html', {'clientes': clientes})
