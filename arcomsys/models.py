442a7a2e9365b014245950d083d1cab3b156ae67
Daniel Fonseca Alves <dalrafgmail.com>
2017-08-29 11:09:41 -0300

    Adicionado modelos inciais

--- arcomsys/models.py
+++ arcomsys/models.py
@@ -4,3 +4,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nome = models.CharField("Nome",max_length=150, blank=True, null=True)
+    
+    def __unicode__(self):
+        return self.nome
+
+class Contato(models.Model):
+    nome = models.CharField("Nome",max_length=150, blank=True, null=True)
+    telefone = models.CharField("Telefone",max_length=12, blank=True, null=True)
+    email = models.CharField("Email",max_length=12, blank=True, null=True)
+    client = models.ForeignKey('Cliente', on_delete=models.CASCADE )
+
+    def __unicode__(self):
+        return self.nome
